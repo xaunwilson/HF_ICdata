90f2a723d78a7f41d0eda3b85b8651fbd06cd8c9
xaunwilson <47012491+xaunwilson@users.noreply.github.com>
2021-06-27 19:55:06 -0700

    Add files via upload

---
+++ Foliar Carbon Data.R
@@ -0,0 +1,57 @@
+library(httr)
+library(jsonlite)
+
+#pull API replace DP identifier with DP identifier from target dataset  
+req <- GET("http://data.neonscience.org/api/v0/products/DP1.10026.001")
+
+#status 200 means success
+
+req.text <- content(req, as="text")
+avail <- jsonlite::fromJSON(req.text, simplifyDataFrame=T, flatten=T)
+
+#show avail to see what it's storing, but output will take up a lot of space in console 
+print(avail)
+
+## view description 
+avail$data$productDescription
+
+## view abstract 
+avail$data$productAbstract
+
+## view available sites 
+avail$data$siteCodes$siteCode
+
+## view site [HARV], for other sites replace 13
+avail$data$siteCodes$siteCode[[10]]
+
+## view available months for site
+avail$data$siteCodes$availableMonths[[10]]
+
+
+
+##downloading using API 
+zipsByProduct(dpID="DP1.10026.001", site="HARV", 
+              startdate="2016-08", enddate="2020-09",
+              package="expanded", avg=30, check.size=T)
+
+##if downloaded from table start here 
+
+stackByTable(filepath="/Users/xaunwilson/Desktop/Harvard Forest/Data/HARV/NEON_traits-foliar.zip")
+
+
+##once dataset is downloaded once use code below to get necessary data 
+
+FOL <- readTableNEON(
+  dataFile='/Users/xaunwilson/Desktop/Harvard Forest/Data/HARV/NEON_traits-foliar/stackedFiles/cfc_carbonNitrogen.csv',
+  varFile='/Users/xaunwilson/Desktop/Harvard Forest/Data/HARV/NEON_traits-foliar/stackedFiles/variables_10026.csv'
+)
+
+HARV.foliar <- data.frame(FOL$plotID, FOL$plotType, 
+                          FOL$collectDate, FOL$co2Trapped, 
+                          FOL$d15N, FOL$d13C, FOL$nitrogenPercent, 
+                          FOL$carbonPercent, FOL$CNratio)
+  
+  
+  
+  
+  
